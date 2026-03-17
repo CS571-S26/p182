62293df51151670d69e07562c4a786c9cf8fde87
cjkundinger <cjkundinger@wisc.edu>
2026-03-17 09:49:09 -0500

    trying to set up project

---
+++ intro.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Intro() {
+    return (
+        <div>
+            <h1> Welcome to my app </h1>
+            <p> Its going to be based on a popular Word Game! </p>
+        </div>
+    );
+}
